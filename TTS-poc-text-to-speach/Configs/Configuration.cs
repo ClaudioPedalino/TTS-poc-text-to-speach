304e20559a9914ab0acc8c7a09263fda96e6ab25
claudiopedalino <claudio.dpedalino@gmail.com>
2022-06-29 02:14:33 -0300

    add libs

---
+++ TTS-poc-text-to-speach/Configs/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using System.Speech.Synthesis;
+
+namespace TTS_poc_text_to_speach.Configs
+{
+    public class Configuration
+    {
+        public static Configuration Build(VoiceGender gender, int rateSpeed)
+            => new Configuration()
+            {
+                Gender = gender,
+                Rate = rateSpeed,
+            };
+
+        public VoiceGender Gender { get; private set; } = VoiceGender.Neutral;
+        public int Rate { get; private set; } = 0;
+
+    }
+}
--- TTS-poc-text-to-speach/Program.cs
+++ TTS-poc-text-to-speach/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Speech.Synthesis;
+using TTS_poc_text_to_speach.Configs;
+
+var configuration = Configuration.Build(VoiceGender.Female, rateSpeed: default);
+
+using (SpeechSynthesizer synth = new SpeechSynthesizer())
+{
+    synth.Speak("Hello World");
+}
+
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
